94eeb223c968053ec5b4f25e50afc3c637b1530b
Olumayowa Taiwo <olumayoor99@gmail.com>
2022-05-26 06:15:39 +0200

    proper finetuning

--- automate_email.py
+++ automate_email.py
@@ -5,19 +5,19 @@ import email
 import yaml
 import os
 import os.path
+import shutil
 
 i = 0
 
 today_date = date.today().strftime("%d %B %Y")
 
-attachment_dir = today_date.replace(' ', '')
-print(f'{attachment_dir}/file1.xlsx')
+att_dir = today_date.replace(' ', '')
 
 with open("credentials.yaml") as f:
     content = f.read()
-my_credentials = yaml.load(content, Loader=yaml.FullLoader)
-user, password, key, value, imap_url, mail_selector = my_credentials["user"], my_credentials["password"], my_credentials[
-    "key"], my_credentials["value"], my_credentials["imap_url"], my_credentials["mail_selector"]
+credentials = yaml.load(content, Loader=yaml.FullLoader)
+user, password, key, value, imap_url, mail_selector = credentials["user"], credentials["password"], credentials[
+    "key"], credentials["value"], credentials["imap_url"], credentials["mail_selector"]
 
 
 my_mail = imaplib.IMAP4_SSL(imap_url)
@@ -35,13 +35,17 @@ for num in mail_id_list:
     typ, data = my_mail.fetch(num, '(RFC822)')
     msgs.append(data)
 
-for msg in msgs[::-1]:
-    for response_part in msg:
-        if type(response_part) is tuple:
-            while (True):
+try:
+    os.mkdir(att_dir)
+except FileExistsError:
+    shutil.rmtree(att_dir)
+    os.mkdir(att_dir)
 
+while i < 2:
+    for msg in msgs[::-1]:
+        for response_part in msg:
+            if type(response_part) is tuple:
                 my_msg = email.message_from_bytes((response_part[1]))
-
                 rcv_date = (date.today() - timedelta(i)).strftime("%d %B %Y")
                 if rcv_date in my_msg["date"]:
                     print("_____________________________")
@@ -56,47 +60,45 @@ for msg in msgs[::-1]:
                             continue
                         if part.get('Content-Disposition') is None:
                             continue
-
                         fileName = part.get_filename()
-                        filePath = os.path.join(attachment_dir, fileName)
-                        f_name = f"file{i+1}.xlsx"
-                        xl_file = os.path.join(attachment_dir, f_name)
+                        filePath = os.path.join(att_dir, fileName)
+                        f_name = f"file{i}.xlsx"
+                        xl_file = os.path.join(att_dir, f_name)
 
                         if bool(fileName):
-                            try:
-                                os.mkdir(attachment_dir)
-                            except Exception:
-                                pass
                             with open(filePath, 'wb') as f:
                                 f.write(part.get_payload(decode=True))
+                                print(
+                                    f"I'm writing the file {fileName} to disk {i} times")
+                                f.close()
                         try:
                             os.rename(filePath, xl_file)
                         except FileExistsError:
                             os.remove(xl_file)
                             os.rename(filePath, xl_file)
-                if i == 2:
-                    break
 
-                i += 1
+    i += 1
 
-df1 = pd.read_excel(f'{attachment_dir}/file1.xlsx')
+xfile1 = pd.read_excel(os.path.join(att_dir, 'file0.xlsx'))
 
-df2 = pd.read_excel(f'{attachment_dir}/file1.xlsx')
+xfile2 = pd.read_excel(os.path.join(att_dir, 'file1.xlsx'))
 
-df_join = df1.merge(right=df2, left_on=df1.columns.to_list(),
-                    right_on=df2.columns.to_list(), how='outer')
+xfile_join = xfile1.merge(right=xfile2, left_on=xfile1.columns.to_list(),
+                          right_on=xfile2.columns.to_list(), how='outer')
 
-df1.rename(columns=lambda x: x + '_file1', inplace=True)
+xfile1.rename(columns=lambda x: x + '_file1', inplace=True)
 
-df2.rename(columns=lambda x: x + '_file2', inplace=True)
+xfile2.rename(columns=lambda x: x + '_file2', inplace=True)
 
-df_join = df1.merge(right=df2, left_on=df1.columns.to_list(),
-                    right_on=df2.columns.to_list(), how='outer')
+xfile_join = xfile1.merge(right=xfile2, left_on=xfile1.columns.to_list(),
+                          right_on=xfile2.columns.to_list(), how='outer')
 
-records_present_in_df1_not_in_df2 = df_join.loc[df_join[df2.columns.to_list(
-)].isnull().all(axis=1), df1.columns.to_list()]
+xfile1_not_xfile2 = xfile_join.loc[xfile_join[xfile2.columns.to_list(
+)].isnull().all(axis=1), xfile1.columns.to_list()]
 
-records_present_in_df2_not_in_df1 = df_join.loc[df_join[df1.columns.to_list(
-)].isnull().all(axis=1), df2.columns.to_list()]
+xfile2_not_xfile1 = xfile_join.loc[xfile_join[xfile1.columns.to_list(
+)].isnull().all(axis=1), xfile2.columns.to_list()]
 
-print(records_present_in_df1_not_in_df2)
+xfile1_not_xfile2.to_excel('new_items.xlsx')
+
+print(xfile1_not_xfile2)
